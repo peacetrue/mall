620579a5ec25f41e0ed32f16cb58c7700dbca1c2
xiayx <xiayouxue@gold32.com>
2021-05-02 22:15:34 +0800

    add react-admin Admin

--- mail-merchant-front/src/App.tsx
+++ mail-merchant-front/src/App.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Admin} from "react-admin";
+import {PeaceSpringDataProviderBuilder} from "peacetrue-react-admin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Admin title="抖音商城"
+           dataProvider={PeaceSpringDataProviderBuilder(process.env.REACT_APP_BASE_URL as string)}>
+
+    </Admin>
   );
 }
 
