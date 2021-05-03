93e1c697605c57e54171a9c333c3a8af9a923fa5
xiayx <xiayouxue@gold32.com>
2021-05-03 12:41:18 +0800

    语言支持

--- mail-merchant-front/src/App.tsx
+++ mail-merchant-front/src/App.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import {Admin, Resource} from "react-admin";
-import {PeaceSpringDataProviderBuilder} from "peacetrue-react-admin";
+import {PeaceEnv, PeaceSpringDataProviderBuilder} from "peacetrue-react-admin";
 import {PeaceAuthProviderBuilder} from "./PeaceAuthProviderBuilder";
 import {AuthProviderHttpClient, DataProviderHttpClient} from "./PeaceHttpClient";
 import {MemberResource} from "peacetrue-member"
 import {OrderResource} from "peacetrue-order";
-import {GoodsResource} from "peacetrue-goods";
+import {GoodsResource} from "./modules/goods";
+// import {GoodsResource} from "/Users/xiayx/Documents/Projects/peacetrue-goods/peacetrue-goods-react-admin/src/modules/goods";
 import {EnumResources} from "./PeaceEnum";
+import {Messages} from "./Messages";
+import polyglotI18nProvider from "ra-i18n-polyglot"
 
 function App() {
-  let url = process.env.REACT_APP_BASE_URL as string;
   return (
     <Admin title="抖音商城"
-           authProvider={PeaceAuthProviderBuilder({url, httpClient: AuthProviderHttpClient})}
-           dataProvider={PeaceSpringDataProviderBuilder(url, DataProviderHttpClient)}
+           authProvider={PeaceAuthProviderBuilder({url: PeaceEnv.apiUrl, httpClient: AuthProviderHttpClient})}
+           dataProvider={PeaceSpringDataProviderBuilder(PeaceEnv.apiUrl, DataProviderHttpClient)}
+           i18nProvider={polyglotI18nProvider(() => Messages, 'cn')}
     >
       {GoodsResource}
       <Resource name={"merchants"}/>
